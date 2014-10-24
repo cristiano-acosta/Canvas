6c68b028ef2c435df1fc70c30e16409cb0993790
Cristiano Acosta <cristiano-acosta@hotmail.com>
2014-10-24 10:44:33 -0200

    Outros Ajustes

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var wow = new WOW(
         {
           boxClass: 'wow', // animated element css class (default is wow)
           animateClass: 'animated', // animation css class (default is animated)
-          offset: 0, // distance to the element when triggering the animation (default is 0)
+          offset: 1, // distance to the element when triggering the animation (default is 0)
           mobile: false, // trigger animations on mobile devices (true is default)
           live: true // act on asynchronously loaded content (default is true)
         }
